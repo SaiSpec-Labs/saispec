071615a55d82b9516f23d91d783a2ca94bfb2a51
SaiSpec-Labs <hellosaispec@gmail.com>
2025-12-14 17:14:32 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saispec",
+    version="0.1.0",
+    description="The Standard AI Governance & Logic Audit Library.",
+    packages=find_packages(), # This finds the 'saispec' folder automatically
+    python_requires='>=3.8',
+)
